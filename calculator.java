a6d52775ebbfbb4f2193d424ab9fd9d9971a38f4
prisha1711 <guptaprisha1711@gmail.com>
2026-01-12 10:43:50 +0530

    day1

---
+++ calculator.java
@@ -0,0 +1,11 @@
+public class calculator {
+        static int add(int a, int b){
+            return a+b;
+        }
+
+        public static void main(String[] args){
+            int a=10, b=15;
+            int sum = add(a,b);
+            System.out.println("Sum = " + sum);
+        }
+}
